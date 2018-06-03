a7b38582d5e6cc7354e0a7d91dff52fbfe23e06d
ChoWonmin <0108257@gmail.com>
2018-06-03 13:14:12 +0900

    upload

--- btn/btn_dev.c
+++ btn/btn_dev.c
@@ -3,36 +3,51 @@
 #include <linux/module.h>
 #include <linux/delay.h>
 #include <linux/fs.h>
+#include <linux/uaccess.h>
 
 #define GPIO1 17
 #define DEV_NAME "btn_dev"
 #define DEV_NUM 240
 
 MODULE_LICENSE("GPL");
+static char *msg = "";
+int res = 0;
 
-int btn_scan(struct inode *pinode, struct file *pfile){
+int btn_open(struct inode *pinode, struct file *pfile){
     
-    int res = -1;
     printk(KERN_ALERT "open btn_dev\n");
     
     gpio_request(GPIO1, "GPIO1");
-    
     gpio_direction_input(GPIO1);
-    
-    while(1) {
-        res = gpio_get_value(GPIO1);
-        
-        if(res){
-            break;
-        }
-        
-    }
     
     return 0;
 }
+
+ssize_t btn_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
+{
+    res = gpio_get_value(GPIO1);
+    if(res)
+        copy_to_user(buffer, "click", length);
+    else
+        copy_to_user(buffer, "no click", length);
+    return 0;
+}
+ssize_t btn_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
+{
+    printk(KERN_ALERT "Write\n");
+    
+    if(copy_from_user(msg, buffer, length)) {
+        printk("write error\n");
+        return -1;
+    }
+    
+    return length;
+}
 struct file_operations fop = {
     .owner = THIS_MODULE,
-    .open = btn_scan,
+    .open = btn_open,
+    .read = btn_read,
+    .write = btn_write,
 };
 
 int __init btn_init(void) {
@@ -47,4 +62,5 @@ void __exit btn_exit(void) {
 
 module_init(btn_init);
 module_exit(btn_exit);
+
 
--- btn/btn_lcd.c
+++ btn/btn_lcd.c
@@ -1,23 +1,30 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <sys/types.h>
+#include <errno.h>
 #include <fcntl.h>
+#include <string.h>
 #include <unistd.h>
+#include <sys/types.h>
 #include <linux/kdev_t.h>
 #include <wiringPi.h>
 #include <lcd.h>
 
-#define LCD_RS 11
-#define LCD_E 10
+#define BUFFER_LENGTH 256
+#define DEV_PATH "/dev/btn_dev"
+
+#define LCD_RS 27
+#define LCD_E 28
 #define LCD_D4 6
 #define LCD_D5 5
 #define LCD_D6 4
 #define LCD_D7 1
 
-#define DEV_PATH "/dev/btn_dev"
+static char receive[BUFFER_LENGTH];
 
-int lcd_init(int lcd)
+int lcd_init()
 {
+    int lcd;
+    
     wiringPiSetup();
     
     if(lcd = lcdInit(2, 16, 4, LCD_RS, LCD_E, LCD_D4, LCD_D5, LCD_D6, LCD_D7, 0, 0, 0, 0)){
@@ -32,41 +39,43 @@ void write_lcd(int lcd, char* str){
     
     lcdPosition(lcd,0,0);
     lcdPuts(lcd, str);
-    
 }
 
-int main(int argc, char *argv[]) {
-    int fd, repeat = 0;
+int main(){
+    int ret, fd;
+    char stringToSend[BUFFER_LENGTH];
     int on = 0;
+    
     int lcd;
     
-    lcd = lcd_init(lcd);
+    lcd = lcd_init();
+    
+    if((fd = open(DEV_PATH, O_RDWR | O_NONBLOCK)) < 0) {
+        perror("open()");
+        exit(1);
+    }
     
     while(1){
-        if((fd = open(DEV_PATH, O_RDWR | O_NONBLOCK)) < 0) {
-            perror("open()");
-            exit(1);
-        }
-        close(fd);
+        ret = read(fd, receive, BUFFER_LENGTH);
         
-        on = on?0:1;
-        printf("on : %d\n",on);
-        
-        if(on) {
-            
-            write_lcd(lcd,"lcd turn on\n");
-            
-            //printf("Process 1: continue , 0: terminate :: \n");
-            //scanf(" %d\n", &repeat);
-            
-            //if(!repeat)
-            //    break;
-        }else {
-            printf("lcd turin off\n");
-            write_lcd(lcd,"off \n");
+        if (ret < 0) {
+            perror("Failed to read... \n");
+            return errno;
         }
         
+        if (strcmp(receive,"click")==0)
+            on = on?0:1;
+        
+        if (on)
+            write_lcd(lcd,"on");
+        else
+            write_lcd(lcd,"        ");
+        
+        sleep(1);
     }
+    
+    close(fd);
     
     return 0;
 }
+
