d239fda4e004234b58eb43867f50d4cc3e098977
ChoWonmin <0108257@gmail.com>
2018-06-03 10:42:54 +0900

    app과 driver가 read, copy_to_user로 통신하도록 변경
    app이 dirver에게 read를 통해 버튼이 눌렀는지 물어본다.
    driver btn_read()에서 받아서 클릭이되면 "click"이라고 보낸다.

--- btn/btn_dev.c
+++ btn/btn_dev.c
@@ -3,48 +3,62 @@
 #include <linux/module.h>
 #include <linux/delay.h>
 #include <linux/fs.h>
+#include <linux/uaccess.h>
 
-#define GPIO1 4
+#define GPIO1 17
 #define DEV_NAME "btn_dev"
 #define DEV_NUM 240
 
 MODULE_LICENSE("GPL");
+static char *msg = "";
+int res = 0; // button 산호의 결과값 1이면 click
 
-int btn_scan(struct inode *pinode, struct file *pfile){
-
-  int res = -1;
-  printk(KERN_ALERT "open btn_dev\n");
-	
-  gpio_request(GPIO1, "GPIO1");
+int btn_open(struct inode *pinode, struct file *pfile){
     
-  gpio_direction_input(GPIO1);
+    printk(KERN_ALERT "open btn_dev\n");
+    
+    gpio_request(GPIO1, "GPIO1");
+    gpio_direction_input(GPIO1); // GPIO1을 input으로 setting
+    
+    return 0;
+}
 
-  while(1) {
-    res = gpio_get_value(GPIO1);
-    if(!res){
-      printk(KERN_DEBUG "===== %d =====\n", res);
-      break;
+ssize_t btn_read(struct file *pfile, char __user *buffer, size_t length, loff_t *offset)
+{
+    res = gpio_get_value(GPIO1); // click이되면 1
+    if(res)
+        copy_to_user(buffer, "click", length); // click이면 app에게 "click"
+    else
+        copy_to_user(buffer, "no click", length);
+    return 0;
+}
+ssize_t btn_write(struct file *pfile, const char __user *buffer, size_t length, loff_t *offset)
+{
+    printk(KERN_ALERT "Write\n");
+    
+    if(copy_from_user(msg, buffer, length)) {
+        printk("write error\n");
+        return -1;
     }
     
-  } 
-
-  return 0;
+    return length;
 }
 struct file_operations fop = {
-	.owner = THIS_MODULE,
-	.open = btn_scan,
+    .owner = THIS_MODULE,
+    .open = btn_open,
+    .read = btn_read,
+    .write = btn_write,
 };
 
 int __init btn_init(void) {
-	printk(KERN_ALERT "init btn get value\n");
-	register_chrdev(DEV_NUM, DEV_NAME, &fop);
-	return 0;
+    printk(KERN_ALERT "init btn get value\n");
+    register_chrdev(DEV_NUM, DEV_NAME, &fop);
+    return 0;
 };
 void __exit btn_exit(void) {
-	printk(KERN_ALERT "exit btn_dev\n");
-	unregister_chrdev(DEV_NUM, DEV_NAME);
+    printk(KERN_ALERT "exit btn_dev\n");
+    unregister_chrdev(DEV_NUM, DEV_NAME);
 }
 
 module_init(btn_init);
 module_exit(btn_exit);
-
